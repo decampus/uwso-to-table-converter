40a24f8418beafdea7347476f78bc470e63892c7
decampus <mos.decampus.cs@gmail.com>
2024-01-06 23:54:58 -0300

    feat: add hasHeader() to Text class

--- public/javascript/classes/Text.js
+++ public/javascript/classes/Text.js
@@ -2,6 +2,13 @@ export class Text {
     constructor(content) {
         this.content = content;
     }
+    hasHeader() {
+        const regex = /Observations|Location|\d\s\w{3}\s\d{4}/gm;
+        const match = regex.exec(this.getContent());
+        if (match)
+            return true;
+        return false;
+    }
     getContent() {
         return this.content;
     }
--- src/typescript/classes/Text.ts
+++ src/typescript/classes/Text.ts
@@ -5,6 +5,17 @@ export class Text {
         this.content = content;
     }
 
+    hasHeader(): boolean {
+        const regex = /Observations|Location|\d\s\w{3}\s\d{4}/gm;
+        const match = regex.exec(this.getContent());
+
+        if (match)
+            return true;
+
+        return false;
+        
+    }
+
     getContent() {
         return this.content;
     }
