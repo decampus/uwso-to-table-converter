dbfe5aaa0010b56005e7915ed964e56318c8c4a0
decampus <mos.decampus.cs@gmail.com>
2024-01-11 23:44:33 -0300

    feat: create Document class

---
+++ public/javascript/classes/Document.js
@@ -0,0 +1,6 @@
+export class Document {
+    constructor(text) {
+        this.text = text;
+    }
+}
+//# sourceMappingURL=Document.js.map
---
+++ src/typescript/classes/Document.ts
@@ -0,0 +1,7 @@
+export class Document {
+    private text: Text;
+
+    constructor(text: Text) {
+        this.text = text;
+    }
+}
