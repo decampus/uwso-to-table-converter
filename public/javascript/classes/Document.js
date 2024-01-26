908acb1346c4a54c7b585fd52d8c30105a1ec491
decampus <mos.decampus.cs@gmail.com>
2024-01-26 10:05:42 -0300

    feat: add downloadFile method to Document

--- public/javascript/classes/Document.js
+++ public/javascript/classes/Document.js
@@ -2,5 +2,25 @@ export class Document {
     constructor(text) {
         this.text = text;
     }
+    splitTextContent() {
+        const newArr = [];
+        for (let line of this.text.getContent()) {
+            newArr.push(line.split(" ").filter(Boolean));
+        }
+        try {
+            newArr[1].unshift('');
+        }
+        finally {
+            return newArr;
+        }
+    }
+    downloadFile() {
+        let content = 'data:text/csv;charset=utf-8;,';
+        this.splitTextContent().forEach(function (row) {
+            let newRow = row.join(',');
+            content += newRow + '\r\n';
+        });
+        window.open(encodeURI(content));
+    }
 }
 //# sourceMappingURL=Document.js.map
--- src/typescript/classes/Document.ts
+++ src/typescript/classes/Document.ts
@@ -1,7 +1,36 @@
+import { Text } from "./Text.js";
+
 export class Document {
     private text: Text;
 
     constructor(text: Text) {
         this.text = text;
+    }
+
+    private splitTextContent(): string[][] {
+        const newArr: string[][] = [];
+
+        for (let line of this.text.getContent()) {
+            newArr.push(line.split(" ").filter(Boolean))
+        }
+
+        try {
+            newArr[1].unshift('');
+        }
+        finally {
+            return newArr;
+        }
+    }
+
+    downloadFile(): void {
+        let content = 'data:text/csv;charset=utf-8;,';
+
+        this.splitTextContent().forEach(function(row) {
+            let newRow = row.join(',');
+
+            content += newRow + '\r\n'
+        })
+
+        window.open(encodeURI(content));
     }
 }
