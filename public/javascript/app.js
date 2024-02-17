0dccdf40fa2a9591b679d2f487ac243e2f88e10f
decampus <mos.decampus.cs@gmail.com>
2024-02-16 23:53:45 -0300

    feat: toggle Download section visibility

--- public/javascript/app.js
+++ public/javascript/app.js
@@ -5,6 +5,7 @@ const convert = document.getElementById('convert-button');
 const clear = document.getElementById('clear-button');
 const download = document.getElementById('download-button');
 const textarea = document.getElementById('textarea');
+const downloadSection = document.getElementById('download-section');
 convert.addEventListener('click', convertToTable);
 clear.addEventListener('click', clearTextarea);
 download.addEventListener('click', downloadFile);
@@ -19,15 +20,24 @@ function convertToTable() {
             text.removeEqualSignLine();
         }
         controller.insertText(text);
+        showDownloadSection();
     }
 }
 function clearTextarea() {
     if (textarea.value.length)
         textarea.value = "";
+    hideDownloadSection();
 }
 function downloadFile() {
     const doc = new Document(controller.getText()[0]);
     controller.insertDocument(doc);
     controller.downloadFile();
+}
+function showDownloadSection() {
+    downloadSection.style.display = "block";
+}
+function hideDownloadSection() {
+    if (downloadSection.style.display === "block")
+        downloadSection.style.display = "none";
 }
 //# sourceMappingURL=app.js.map
--- src/typescript/app.ts
+++ src/typescript/app.ts
@@ -8,6 +8,8 @@ const clear = document.getElementById('clear-button') as HTMLButtonElement;
 const download = document.getElementById('download-button') as HTMLButtonElement;
 const textarea = document.getElementById('textarea') as HTMLTextAreaElement;
 
+const downloadSection = document.getElementById('download-section') as HTMLDivElement;
+
 convert.addEventListener('click', convertToTable);
 clear.addEventListener('click', clearTextarea);
 download.addEventListener('click', downloadFile)
@@ -27,16 +29,29 @@ function convertToTable() {
         }
 
         controller.insertText(text);
+
+        showDownloadSection();
     }
 }
 
 function clearTextarea() {
     if (textarea.value.length)
         textarea.value = "";
+
+    hideDownloadSection();
 }
 
 function downloadFile() {
     const doc = new Document(controller.getText()[0]);
     controller.insertDocument(doc);
     controller.downloadFile();
+}
+
+function showDownloadSection() {
+    downloadSection.style.display = "block";
+}
+
+function hideDownloadSection() {
+    if (downloadSection.style.display === "block")
+        downloadSection.style.display = "none";
 }
